05ee1e7d42f86272ec9472c962e3744fc165e446
Ecomwebfly <ajinathkashid@gmail.com>
2019-10-25 11:05:03 +0530

    new API Project

--- SHF.API/Controllers/ValuesController.cs
+++ SHF.API/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ using System.Web.Http;
 
 namespace SHF.API.Controllers
 {
-    [Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
--- SHF.API/Properties/AssemblyInfo.cs
+++ SHF.API/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6f0bd7ed-3bdc-4499-be1b-f8a5f645a45b")]
+[assembly: Guid("9afef017-d3db-42ee-806b-9ee10129a59b")]
 
 // Version information for an assembly consists of the following four values:
 //
