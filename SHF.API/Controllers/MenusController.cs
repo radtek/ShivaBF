732b11c770c50eb4783c954583b039fa9c973cd5
MTDEV16 <MTDEV16@MTDEV16>
2019-10-25 09:33:30 -0400

    Menus Api Added

---
+++ SHF.API/Controllers/MenusController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Reflection;
+using System.ComponentModel;
+using System.Web.Http.Results;
+using SHF.ViewModel.FrontEnd;
+
+
+namespace SHF.API.Controllers
+{
+    public class MenusController : ApiController
+    {
+        #region [Field & Contructor]
+
+        private Business.Interface.IMessage businessMessage;
+        private Business.Interface.ICategoriesMaster businessCategoriesMaster;
+        private Business.Interface.ISubCategoriesMaster businessSubCategoriesMaster;
+        private Business.Interface.ISubSubCategoriesMaster businessSubSubCategoriesMaster;
+
+        public MenusController(Business.Interface.IMessage Imessage, Business.Interface.ICategoriesMaster ICategoriesMaster, Business.Interface.ISubCategoriesMaster ISubCategoriesMaster, Business.Interface.ISubSubCategoriesMaster ISubSubCategoriesMaster)
+        {
+            this.businessMessage = Imessage;
+            this.businessCategoriesMaster = ICategoriesMaster;
+            this.businessSubCategoriesMaster = ISubCategoriesMaster;
+            this.businessSubSubCategoriesMaster = ISubSubCategoriesMaster;
+
+        }
+        // GET: api/GetAllActiveMenusByTenantId?tenantId=1
+        [HttpGet]
+        [Route("api/GetAllActiveMenusByTenantId")]
+        public JsonResult<MenusMasterIndexViewModel> GetAllActiveMenusByTenantId(string tenantId)
+        {
+            var MenusMasterIndexViewModel = new MenusMasterIndexViewModel();
+            var categories = this.businessCategoriesMaster.GetAll().Where(x => x.Tenant_ID == Convert.ToInt64(tenantId) && x.DisplayOnHome == true && x.IsActive == true).OrderBy(a => a.DisplayIndex);
+            foreach (var tempcat in categories)
+            {
+                var MenusMasterCategoryIndexViewModel = new MenusMasterCategoryIndexViewModel();
+                MenusMasterCategoryIndexViewModel.ID = tempcat.ID;
+                MenusMasterCategoryIndexViewModel.CategoryName = tempcat.CategoryName;
+                MenusMasterCategoryIndexViewModel.DisplayIndex = tempcat.DisplayIndex;
+                MenusMasterCategoryIndexViewModel.Tenant_ID = Convert.ToInt64(tempcat.Tenant_ID);
+                MenusMasterIndexViewModel.MenusMasterCategoryIndexViewModel.Add(MenusMasterCategoryIndexViewModel);
+                var subcategories = this.businessSubCategoriesMaster.GetAll().Where(x => x.Tenant_ID == Convert.ToInt64(tenantId) && x.DisplayOnHome == true && x.IsActive == true && x.Cat_Id == Convert.ToInt64(tempcat.ID)).OrderBy(a => a.DisplayIndex);
+                foreach (var tempsubcat in subcategories)
+                {
+                    var MenusMasterSubCategoryIndexViewModel = new MenusMasterSubCategoryIndexViewModel();
+                    MenusMasterSubCategoryIndexViewModel.ID = tempsubcat.ID;
+                    MenusMasterSubCategoryIndexViewModel.SubCategoryName = tempsubcat.SubCategoryName;
+                    MenusMasterSubCategoryIndexViewModel.DisplayIndex = tempsubcat.DisplayIndex;
+                    MenusMasterSubCategoryIndexViewModel.Category_ID = Convert.ToInt64(tempsubcat.Cat_Id);
+                    MenusMasterSubCategoryIndexViewModel.Tenant_ID = Convert.ToInt64(tempsubcat.Tenant_ID);
+                    MenusMasterCategoryIndexViewModel.MenusMasterSubCategoryViewModel.Add(MenusMasterSubCategoryIndexViewModel);
+                    var subsubcategories = this.businessSubSubCategoriesMaster.GetAll().Where(x => x.Tenant_ID == Convert.ToInt64(tenantId) && x.DisplayOnHome == true && x.IsActive == true && x.SubCat_Id == Convert.ToInt64(tempsubcat.ID)).OrderBy(a => a.DisplayIndex);
+                    foreach (var tempsubsubcat in subsubcategories)
+                    {
+                        var MenusMasterSubSubCategoryIndexViewModel = new MenusMasterSubSubCategoryIndexViewModel();
+                        MenusMasterSubSubCategoryIndexViewModel.ID = tempsubsubcat.ID;
+                        MenusMasterSubSubCategoryIndexViewModel.SubSubCategoryName = tempsubsubcat.SubSubCategoryName;
+                        MenusMasterSubSubCategoryIndexViewModel.DisplayIndex = tempsubsubcat.DisplayIndex;
+                        MenusMasterSubSubCategoryIndexViewModel.ServiceTypeValue = tempsubsubcat.ServiceTypeValue;
+                        MenusMasterSubSubCategoryIndexViewModel.Category_ID = Convert.ToInt64(tempsubsubcat.Cat_Id);
+                        MenusMasterSubSubCategoryIndexViewModel.SubCategory_ID = Convert.ToInt64(tempsubsubcat.SubCat_Id);
+                        MenusMasterSubSubCategoryIndexViewModel.Tenant_ID = Convert.ToInt64(tempsubsubcat.Tenant_ID);
+                        MenusMasterSubCategoryIndexViewModel.MenusMasterSubSubCategoryViewModel.Add(MenusMasterSubSubCategoryIndexViewModel);
+
+                    }
+                }
+
+            }
+            /*some db operation*/
+            return Json(MenusMasterIndexViewModel);
+        }
+        #endregion
+    }
+
+}
---
+++ SHF.ViewModel/FrontEnd/MenusMasterViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SHF.ViewModel.FrontEnd
+{
+
+    public sealed class MenusMasterIndexViewModel : BaseViewModel
+    {
+
+
+        public List<ViewModel.FrontEnd.MenusMasterCategoryIndexViewModel> MenusMasterCategoryIndexViewModel { get; set; }
+    }
+    public sealed class MenusMasterCategoryIndexViewModel : BaseViewModel
+    {
+
+      
+        public System.Int64 Cat_Id { get; set; }
+        public System.String CategoryName { get; set; }
+         public System.Int32 DisplayIndex { get; set; }
+        public System.Boolean? DisplayOnHome { get; set; }
+        public System.Boolean? IsActive { get; set; }
+        public System.Int32 TotalViews { get; set; }
+        public System.String TenantName { get; set; }
+        public System.Int64 Tenant_ID { get; set; }
+        public List<ViewModel.FrontEnd.MenusMasterSubCategoryIndexViewModel> MenusMasterSubCategoryViewModel { get; set; }
+    }
+    public sealed class MenusMasterSubCategoryIndexViewModel : BaseViewModel
+    {
+        public System.Int64 SubCategory_ID { get; set; }
+        public System.String SubCategoryName { get; set; }
+        public System.Int64 Category_ID { get; set; }
+        public System.Int32 DisplayIndex { get; set; }
+        public System.Int64 Tenant_ID { get; set; }
+        public List<ViewModel.FrontEnd.MenusMasterSubSubCategoryIndexViewModel> MenusMasterSubSubCategoryViewModel { get; set; }
+    }
+
+    public sealed class MenusMasterSubSubCategoryIndexViewModel : BaseViewModel
+    {
+        public System.String SubSubCategoryName { get; set; }
+        public System.Int64 SubSubCategory_ID { get; set; }
+        public System.Int64 SubCategory_ID { get; set; }
+        public System.Int64 Category_ID { get; set; }
+        public System.String ServiceTypeValue { get; set; }
+        public System.Int32 DisplayIndex { get; set; }
+        public System.Int64 Tenant_ID { get; set; }
+    }
+}
+
