089977692e488b3d5f927a8fbe419452ef961ee4
MTDEV16 <MTDEV16@MTDEV16>
2019-11-26 08:13:56 -0500

    Delete Service 1

---
+++ SHF.DB/Scripts/PostDeployment/SP/usp_DeleteService1.sql
@@ -0,0 +1,26 @@
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE name = 'usp_DeleteService1')  
+DROP PROCEDURE dbo.usp_DeleteService1  
+GO
+CREATE PROCEDURE [dbo].[usp_DeleteService1]
+(
+@serviceId BIGINT=NULL
+)
+AS
+BEGIN
+DECLARE @NumDeleted AS int;
+    SET @NumDeleted = 0;
+	SET NOCOUNT ON;
+	 BEGIN TRANSACTION ucDelCand
+	delete from Tbl_Services1Section10BankMapping where Service_Id=@serviceId;
+	delete from Tbl_Services1Section1Master where Service_Id=@serviceId;
+	delete from Tbl_Services1Section4Master where Service_Id=@serviceId;
+	delete from Tbl_Services1Section5Master where Service_Id=@serviceId;
+	delete from Tbl_Services1Section6PriceMaster where Service_Id=@serviceId;
+	delete from Tbl_Services1Section8FAQMapping where Service_Id=@serviceId;
+	SET @NumDeleted = @@ROWCOUNT;
+
+    COMMIT TRANSACTION ucDelCand
+
+    SELECT @NumDeleted;
+END
+Go
